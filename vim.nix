0f281560fdf02296ea0485dd76d309784620b10f
0kaguya <carbonc014@gmail.com>
2022-01-01 06:27:48 +0000

    rearrange file layout, fix the color problem of GitGutter.

--- vim.nix
+++ vim.nix
@@ -1,7 +1,20 @@
+## Used in `system.systemPackages` in 'configuration.nix'
 with import <nixpkgs> {};
 vim_configurable.customize {
   name = "vim";
+
+  vimrcConfig.packages.myVimPackage = with pkgs.vimPlugins; {
+    start = [
+      vim-gitgutter # some git diff
+      vim-surround
+      # syntax supports
+      vim-nix
+    ];
+  };
+
   vimrcConfig.customRC = ''
+    set nocompatible
+
     set tabstop=4
     set shiftwidth=4
     set expandtab
@@ -9,7 +22,6 @@ vim_configurable.customize {
     set noundofile
     set nobackup
     set backspace=indent,eol,start
-    set nocompatible
 
     filetype plugin indent on
     syntax on
@@ -18,15 +30,10 @@ vim_configurable.customize {
     set cursorline
     highlight CursorLine cterm=NONE
     highlight CursorLineNr ctermfg=blue
+    " set GitGutter sign column color
+    " fixed the problem that the column has grey background.
+    highlight SignColumn ctermbg=NONE
     
     set clipboard=unnamedplus   
   '';
-
-  vimrcConfig.packages.myVimPackage = with pkgs.vimPlugins; {
-    start = [
-      vim-nix
-      vim-gitgutter
-      vim-surround
-    ];
-  };
 }
